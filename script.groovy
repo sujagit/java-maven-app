14a1a7dcfd74185ca6b7de5bc191151ce8f3dbca
sujagit <98834191+sujagit@users.noreply.github.com>
2022-02-24 10:54:30 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,12 +1,3 @@
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerHub_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t sujadocker14/java-maven-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push sujadocker14/java-maven-app:jma-2.0'
-    }
-} 
-
 
 def deployApp() {
     echo 'deploying the application...'
